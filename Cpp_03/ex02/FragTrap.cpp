9a2e38d58d7631f69a829e369c3fde412220e4d1
Crmorale <152791634+Crmorale@users.noreply.github.com>
2026-03-11 01:35:29 +0100

    Fixed FragTrap copy constructor
    Remove redundant assignment in copy constructor.

--- Cpp_03/ex02/FragTrap.cpp
+++ Cpp_03/ex02/FragTrap.cpp
@@ -27,7 +27,6 @@ FragTrap::FragTrap(const std::string name) : ClapTrap(name) {
 	
 FragTrap::FragTrap(const FragTrap &other)  : ClapTrap(other){
 	std::cout << "FragTrap copy constructor called" << std::endl;
-	*this = other;
 	return ;
 }
 
